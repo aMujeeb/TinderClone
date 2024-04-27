a5914ff317cdbe52bc6ad7a2f86f662bbe0a8af4
Ahamed <mohamed_marzook_ahamed_mujeeb_from.scientec@stb.gov.sg>
2024-04-27 21:47:44 +0800

    Initial Code Upload

---
+++ TinderClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinderClone
+//
+//  Created by Ahamed Mujeeb on 27/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Ahamed Mujeeb on 27/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
